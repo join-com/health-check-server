484c6d47546b84a2f5a3210a9ed4c82c8a3ad32f
Aleksey Bobyr <aleksey.bobyr@join.com>
2021-09-07 11:39:08 +0300

    * update dependencies  * update Prettier config & apply it  * fix start function - move handling errors from .listen callback (that doesn't accept error arguments) to an 'error' event listener

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,29 +1,22 @@
 /// <reference types="node" />
-import * as http from 'http';
+import * as http from 'http'
 interface Logger {
-  info: (message: string, payload?: any) => void;
-  error: (message: string, payload?: any) => void;
+  info: (message: string, payload?: any) => void
+  error: (message: string, payload?: any) => void
 }
-declare type PromiseFn = () => Promise<any>;
+declare type PromiseFn = () => Promise<any>
 interface Callbacks {
-  health: PromiseFn;
-  readiness: PromiseFn;
+  health: PromiseFn
+  readiness: PromiseFn
 }
 interface ShutdownState {
-  isShutdown: boolean;
+  isShutdown: boolean
 }
 export declare const createRequestHandler: (
   health: PromiseFn,
   readiness: PromiseFn,
   shutdownState: ShutdownState,
-) => (request: http.IncomingMessage, response: http.ServerResponse) => void;
-export declare const start: (
-  server: http.Server,
-  port: number,
-  logger?: Logger | undefined,
-) => void;
-export declare const createHealthServer: (
-  { health, readiness }: Callbacks,
-  shutdownState: ShutdownState,
-) => http.Server;
-export default createHealthServer;
+) => (request: http.IncomingMessage, response: http.ServerResponse) => void
+export declare const start: (server: http.Server, port: number, logger?: Logger | undefined) => void
+export declare const createHealthServer: ({ health, readiness }: Callbacks, shutdownState: ShutdownState) => http.Server
+export default createHealthServer
